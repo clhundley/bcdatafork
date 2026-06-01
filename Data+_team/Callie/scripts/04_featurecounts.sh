872e420ef095a60afba035f8fd892015836ed7ac
Calista Hundley <clh162@dcc-dhvimdcore-gpu-ferc-s-p15-15.rc.duke.edu>
2026-06-01 11:03:39 -0400

    troubleshooting script

---
+++ Data+_team/Callie/scripts/04_featurecounts.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+#SBATCH --job-name=featureCounts_array
+#SBATCH --time=7-00:00:00
+#SBATCH --array=1-18
+#SBATCH --output=/work/clh162/OysterRNA24/logs/featureCounts_gff3_%A_%a.out
+#SBATCH --error=/work/clh162/OysterRNA24/logs/featureCounts_gff3_%A_%a.err
+#SBATCH --partition=common
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=callie.hundley@duke.edu
+
+## Load module ##
+module load Subread
+
+## Set paths ## 
+GENOME=/work/clh162/OysterRNA24/hisat2_align/Cv_genome_RU_2025_shared
+BAM_DIR=/work/clh162/OysterRNA24/hisat2_align/alignedreads_bam
+COUNT_DIR=/work/clh162/OysterRNA24/hisat2_align/counts
+mkdir -p ${COUNT_DIR}
+
+## Set up direction/path to each sample ##
+# Make list of trimmed sample names (without _R1/_R2 suffix)
+SAMPLES=($(ls ${BAM_DIR}/*_sorted.bam | sed 's/_sorted.bam//' | xargs -n 1 basename))
+
+# Index an individual sample from the list for this array task
+SAMPLE=${SAMPLES[$SLURM_ARRAY_TASK_ID-1]}
+
+## Run featureCounts for gene quantification ##
+echo "Running featureCounts for sample" ${SAMPLE} 
+echo "Using file: ${BAM_DIR}/${SAMPLE}_sorted.bam"
+
+featureCounts \
+    -T ${SLURM_CPUS_PER_TASK} \
+    -p --countReadPairs -B \
+    -a ${GENOME}/*.gtf \
+    -o ${COUNT_DIR}/${SAMPLE}_gene_counts.txt \
+    ${BAM_DIR}/${SAMPLE}_sorted.bam   
+
+echo "featureCounts completed for sample" ${SAMPLE}
