7a53db93606dad1212dba65b4700bd5da297c141
Calista Hundley <clh162@dcc-core-ferc-u-ab39-1-7.rc.duke.edu>
2026-07-01 13:41:22 -0400

    backing up scripts 7/1 progress

--- Data+_team/Callie/scripts/06_diamond.sh
+++ Data+_team/Callie/scripts/06_diamond.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 #SBATCH --job-name=diamond_Cvi3.0_to_Cvi_RU25
 #SBATCH --time=1-00:00:00
-#SBATCH --output=/work/clh162/OysterRNA24/logs/diamond_Cvi3.0_to_Cvi_RU25_%A.out
-#SBATCH --error=/work/clh162/OysterRNA24/logs/diamond_Cvi3.0_to_Cvi_RU25_%A.err
+#SBATCH --output=/work/clh162/OysterRNA24/logs/diamond_Cvi_Yale25_to_Cvi_RU25_%A.out
+#SBATCH --error=/work/clh162/OysterRNA24/logs/diamond_Cvi_Yale25_to_Cvi_RU25_%A.err
 #SBATCH --partition=common
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
@@ -15,21 +15,23 @@ source /hpc/home/clh162/miniconda3/etc/profile.d/conda.sh
 conda activate RNA-seq
 
 ## Set paths ##
-OLD_PROTEOME=/work/clh162/OysterRNA24/DEG_analysis/DESeq2/Cvi_3.0_GCF_002022765.2/protein.faa
+OLD_PROTEOME=/work/clh162/OysterRNA24/DEG_analysis/GO/Cvi_RU17_ncbi_dataset/data/GCF_002022765.2/protein.faa
 NEW_PROTEOME=/work/clh162/OysterRNA24/hisat2_align/Cv_genome_RU_2025_shared/Cvi_RU25.pep
-DMND_DB=/work/clh162/OysterRNA24/DEG_analysis/DESeq2/Cvi_RU25proteome.dmnd
-DIR_OUT=/work/clh162/OysterRNA24/DEG_analysis/DESeq2/
+DIR_OUT=/work/clh162/OysterRNA24/DEG_analysis/GO/diamond/
+
+## Make path directory ##
+mkdir -p ${DIR_OUT}
 
 ## Create diamond index using new proteome to blast against the old proteome ##
 diamond makedb \
-    --in ${NEW_PROTEOME} \
-    -d ${DIR_OUT}/Cvi_RU25proteome
+    --in ${OLD_PROTEOME} \
+    -d ${DIR_OUT}/Cvi_Yale25_db
 
 ## Run diamond blastp ## 
 diamond blastp \
-    -q ${OLD_PROTEOME} \
-    --db ${DMND_DB} \
-    -out ${DIR_OUT_OUT}/old_to_new_diamond.tsv \
+    -q ${NEW_PROTEOME} \
+    --db ${DIR_OUT}/Cvi_Yale25_db.dmnd \
+    -out ${DIR_OUT}/old_to_new_diamond.tsv \
     --evalue 1e-10 \
     --max-target-seqs 1 \
     --ultra-sensitive \
